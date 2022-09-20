e9aba2141c0d49f0232ce7352d8ad902c0861af6
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-09-20 13:34:17 -0300

    atualização de código

--- assets/script.js
+++ assets/script.js
@@ -8,11 +8,17 @@ const relogio = setInterval(function time() {
     let  min  = dateToday.getMinutes();
     let  seg = dateToday.getSeconds();
 
+
+
+
     if(hr <10) hr = '0' + hr
 
     if(min <10) min = '0' + min
 
     if(seg <10) seg = '0' + seg
+
+
+
 
     horas.textContent = hr;
     minutos.textContent = min;
